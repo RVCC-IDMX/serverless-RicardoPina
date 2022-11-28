dc4931f277fbd83d19b8b050f65cf9d2cfceb49a
Ricardo Pina <pinazelaya@gmail.com>
2022-11-27 21:36:23 -0500

    Add pokedex function

---
+++ netlify/functions/hello-world.js
@@ -0,0 +1,9 @@
+/* eslint-disable func-names */
+exports.handler = async function () {
+  return {
+    statusCode: 200,
+    body: JSON.stringify({
+      message: 'Hello world!',
+    }),
+  };
+};
